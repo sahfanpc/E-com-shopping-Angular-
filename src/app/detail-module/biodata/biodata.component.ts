59227fab773b711a9fe13aa89ad97336b858c6d9
sahfanpc <163sahfan@gmail.com>
2023-07-04 10:50:43 -0700

    new

--- src/app/detail-module/biodata/biodata.component.ts
+++ src/app/detail-module/biodata/biodata.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./biodata.component.scss']
 })
 export class BiodataComponent {
-
+  showFiller = false;
 }
--- src/app/detail-module/detail-module.module.ts
+++ src/app/detail-module/detail-module.module.ts
@@ -12,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { AdminComponent } from './admin/admin.component';
 import { BiodataComponent } from './biodata/biodata.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 @NgModule({
   declarations: [LoginComponent, ShoppingDashboardComponent, AdminComponent, BiodataComponent],
   imports: [
@@ -26,6 +27,7 @@ import { BiodataComponent } from './biodata/biodata.component';
     RouterLink,
     MatCardModule,
     MatDividerModule,
+    MatSidenavModule
   ],
   exports: [ShoppingDashboardComponent],
 })
